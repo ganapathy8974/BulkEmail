828a039a3de63a0a067d785ff76b98b1cd0e8e01
Ganu <Ganu@GANU-PC>
2021-07-18 22:23:51 +0530

    Email Project

---
+++ Email/src/bulkemail/SendEmail.java
@@ -0,0 +1,69 @@
+package bulkemail;
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmail {	
+	Session session;    
+    private final String from = "ganapathy8974@gmail.com";
+    private final String password = "N@ncy-8974";
+    //Authentication Method this is create a session once successfully authenticated 
+    public void MailAuthentication() {
+    	
+        String host = "smtp.gmail.com";    
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object.// and pass username and password
+       this.session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication(from, password);
+
+            }
+
+        });
+       
+        session.setDebug(false);
+    }
+    //This Method Contain Email Messages
+    public void EmailMessage(String to, String name) {
+
+        try {            
+            MimeMessage message = new MimeMessage(session);            
+            message.setFrom(new InternetAddress(from));           
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+           
+            message.setSubject("This is Test Mail");    
+          
+            message.setText("Dear "+name+"\n\nGreetings to showing interest!!! \n"
+            		+ "This is My Git Hub URL - https://github.com/ganapathy8974\n"
+            		+ "Follow me in Git then you can esaly get my codes(Free Licence).\n\n"
+            		+ "Thanks & Regards, \r\n"
+            		+ "S.Ganapathy Ramasubramanian,\r\n"
+            		+ "Mobile: 1123456789,");
+
+            System.out.print("Email Sending to "+to);
+            // Send message
+            Transport.send(message);            
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
+    } 
+
+}
---
+++ Email/src/bulkemail/SetEmail.java
@@ -0,0 +1,71 @@
+package bulkemail;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+public class SetEmail {	
+	
+	//-------------------------------Main method------------------------------------------------------------------
+		public static void main(String[] args) {		
+			SendEmail send  = new SendEmail();
+			SetEmail set = new SetEmail(send);		
+			String email_list =  "ganapathy8974.v2@gmail.com,barathdhoni851@gmail.com,barathdhoni851@gmail.com,esakkirajesh005@gmail.com";
+			set.splitEmails(email_list);
+			set.excite();
+		}		
+	//-------------------------------Main method------------------------------------------------------------------
+		
+	SendEmail send;
+	public SetEmail(SendEmail send) {
+		this.send = send;
+	}
+	Set<String> emails = new TreeSet<String>();
+	int valid_email_count;
+	public void splitEmails(String email_list) {
+		int invalid_address = 0;
+		String []emails = email_list.split(",");
+		for(String str : emails) {
+			if(isValidEmailAddress(str)) {
+				this.emails.add(str);
+			}else {
+				System.out.println("Invalid Email Address - " + str);
+				invalid_address++;
+			}
+		}	
+		valid_email_count = this.emails.size();
+		System.out.println((emails.length - valid_email_count)-invalid_address + " Duplicate Emails Given \n" + invalid_address + " Invalid Address Given");	
+		System.out.println(valid_email_count + " Valid Email Address Given and ");
+		System.out.println("Mails Send those valid Email addresses only. \n");
+	}
+	
+	public void excite() {
+		Iterator<String> iter = emails.iterator();
+		String []name;
+		String email;
+		int i=1;
+		send.MailAuthentication();
+		while(iter.hasNext()) {			
+			email = iter.next();
+			name = email.split("@");
+			send.EmailMessage(email,name[0]);
+			System.out.println(" = Done("+ i +"/"+valid_email_count+")");
+			i++;
+		}
+		System.out.println(valid_email_count + " mails successfully sent.");
+	}
+	
+	
+public boolean isValidEmailAddress(String email) {
+		   boolean result = true;
+		   try {
+		      InternetAddress emailAddr = new InternetAddress(email);
+		      emailAddr.validate();
+		   } catch (AddressException ex) {
+		      result = false;
+		   }
+		   return result;
+	} 
+}
